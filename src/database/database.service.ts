74de86f197a742edcf698ae71e4958c1ba2ff8c2
EnriqueV <kalienrique40@gmail.com>
2022-03-10 10:39:41 -0600

    refactor: update database.service

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,33 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from 'dotenv';
-import { parse } from 'path/posix';
-import { Configuration } from '../config/config.key';
-import { ConfigModule } from '../config/config.module';
-import { ConfigService } from '../config/config.service';
-import { ConnectionOptions } from 'typeorm';
+import { configService } from '../config/config.service';
+
+export const typeormconfig = {};
 
 export const databaseProviders = [
-  TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    async useFactory(confi: ConfigService) {
-      return {
-        ssl: true,
-        extra: {
-          ssl: {
-            "rejectUnauthorized": false
-          }
-        },
-        type: 'postgres' as 'postgres',
-        host: confi.get(Configuration.HOST),
-        username: confi.get(Configuration.USERNAME),
-        password: confi.get(Configuration.PASSWORD),
-        database: confi.get(Configuration.DATABASE),
-        port: 25060,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        subscribers: [__dirname + '/../subscribers/*.subscriber{.ts,.js}'],
-      } as ConnectionOptions;
-    },
-  }),
+  TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
 ];
