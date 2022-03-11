7f4cdeab18afac28137ce2d142300cbd1c8e1890
EnriqueV <kalienrique40@gmail.com>
2022-03-11 16:59:18 -0600

    refactor: update users.controller.spec

--- src/modules/users/users/users.controller.spec.ts
+++ src/modules/users/users/users.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from '../users.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([User])],
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            find: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
