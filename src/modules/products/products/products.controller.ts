32cdf62455c826f6fbd051fc399e6a154aa4e61b
EnriqueV <kalienrique40@gmail.com>
2022-03-11 16:34:31 -0600

    refactor: replace product entity to products dto

--- src/modules/products/products/products.controller.ts
+++ src/modules/products/products/products.controller.ts
@@ -1,60 +1,54 @@
-import {Body, Controller,Get, Param, ParseIntPipe, Post  } from '@nestjs/common';
-import {  Product } from '../products.entity';
-import {  ProductsService } from './products.service';
-import { getManager } from 'typeorm';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ProductDTO } from '../dto/products.dto';
+import { ProductInputDTO } from '../dto/product-input.dto';
+import { ProductsService } from './products.service';
 @Controller('products')
 export class ProductsController {
-    private readonly manager = getManager();
+  constructor(private readonly _productService: ProductsService) {}
 
-    constructor(private readonly _productService: ProductsService) {
+  // endpoint get one
+  @Get('getproduct/:id')
+  async getOneuser(@Param('id', ParseIntPipe) id: number): Promise<ProductDTO> {
+    return await this._productService.get(id);
+  }
 
-    }
+  // endpoint get all
+  @Get('getAll')
+  async allproducts(): Promise<ProductDTO[]> {
+    return await this._productService.getAll();
+  }
 
-      // endpoint get one 
-      @Get('getproduct/:id')
-      async getOneuser(@Param('id', ParseIntPipe) id: number): Promise<Product> {
-          const product = await this._productService.get(id);
-          return product
-      }
-  
-      // endpoint get all 
-      @Get('getAll')
-      async allproducts(): Promise<Product[]> {
-  
-          const products = await this._productService.getAll();
-          return products;
-      }
-  
-      @Post()
-      async save(@Body() product: Product) {
-          const p = await this._productService.create(product);
-          return p;
-      }
+  @Post()
+  async save(@Body() product: ProductInputDTO): Promise<ProductDTO> {
+    return await this._productService.create(product);
+  }
 
+  // endpoint get one
+  @Get('updateStatus/:id/:status')
+  async updateStatus(@Param() params) {
+    const product = await this._productService.updateStatus(
+      params.id,
+      params.status,
+    );
+    return product;
+  }
 
-      // endpoint get one 
-      @Get('updateStatus/:id/:status')
-      async updateStatus(@Param() params){
-        const product= await this._productService.updateStatus(params.id, params.status);
-        return product;
-    }
+  @Post('update/:id')
+  async updateProduct(@Param() params, @Body() product: ProductInputDTO) {
+    const update = await this._productService.edit(params.id, product);
+    return update;
+  }
 
-
-    @Post('update/:id')
-    async updateProduct(@Param() params, @Body() product: Product) {
-        const update = await this._productService.edit(params.id, product);
-        return update;
-    }
-
-
-    @Get('delete/:id')
-    async deleteProduct(@Param() params){
-        const product = await this._productService.deleteProduct(params.id);
-        return product;
-    }
-
-
-
-
-
+  @Get('delete/:id')
+  async deleteProduct(@Param() params) {
+    const product = await this._productService.deleteProduct(params.id);
+    return product;
+  }
 }
--- src/modules/products/products/products.service.ts
+++ src/modules/products/products/products.service.ts
@@ -1,83 +1,73 @@
-
-import { BadGatewayException, BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, getManager } from 'typeorm';
 import { Product } from '../products.entity';
+import { ProductDTO } from '../dto/products.dto';
+import { ProductInputDTO } from '../dto/product-input.dto';
 import { ProductRepository } from '../products.repository';
-import { getManager } from 'typeorm';
 @Injectable()
 export class ProductsService {
+  private readonly manager = getManager();
+  constructor(
+    @InjectRepository(Product)
+    private readonly _productRepository: ProductRepository,
+  ) {}
 
-    private readonly manager = getManager();
-    constructor(
-        @InjectRepository(ProductRepository)
-        private readonly _productRepository: ProductRepository,
-
-    ) { }
-
-    async get(id: number): Promise<Product> {
-        if (!id) {
-            throw new BadRequestException('product id is not specified');
-        }
-
-        const product = await this._productRepository.findOne(
-            {
-                where:
-                    { id: id }
-            }
-        );
-        if (!product) {
-            throw new NotFoundException('product not found');
-        }
-
-        return product;
+  async get(id: number): Promise<ProductDTO> {
+    if (!id) {
+      throw new BadRequestException('product id is not specified');
+    }
+    const product = await this._productRepository.findOne({
+      where: { id: id },
+    });
+    if (!product) {
+      throw new NotFoundException('product not found');
     }
 
-    async getAll(): Promise<Product[]> {
+    return product;
+  }
 
-        const product: Product[] = await this._productRepository.find({
-            where: { status: true }
-        });
+  async getAll(): Promise<ProductDTO[]> {
+    return await this._productRepository.find({
+      where: { status: true },
+    });
+  }
 
+  async create(product: ProductInputDTO): Promise<ProductDTO> {
+    const p = await this._productRepository.save(product);
+    return p;
+  }
 
-        return product;
+  async updateStatus(id: number, status: boolean) {
+    const updateCode = this.manager.query(
+      `update products set status =${status} where id = $1`,
+      [id],
+    );
+
+    if (updateCode) {
+      return 'update succesfull';
     }
 
+    throw new BadRequestException('update error');
+  }
 
-    async create(product: Product): Promise<Product> {
-        const saveProduct: Product = await this._productRepository.save(product);
-        return saveProduct;
+  async edit(id: number, producto: ProductInputDTO) {
+    const update = await this._productRepository.update({ id: id }, producto);
+    return update;
+  }
 
-    }
+  async deleteProduct(id: number): Promise<DeleteResult> {
+    const product = await this.manager
+      .createQueryBuilder()
+      .delete()
+      .from(Product)
+      .where('id= :id', { id: id })
+      .execute();
 
-
-    async updateStatus(id:number, status:boolean) {
-
-        const updateCode = this.manager.query(`update products set status =${status} where id = $1`, [id])
-
-          if (updateCode) {
-              return 'update succesfull';
-          }
-
-          throw new BadRequestException('update error');
-}
-
-
-        async edit(id, producto: Product) {
-            const update = await this._productRepository.update({ id: id }, producto)
-            return update;
-        }
-
-
-    async deleteProduct(id){
-            const product = await this.manager.createQueryBuilder().delete().from(Product).where(
-                "id= :id", { id: id },
-            )
-                .execute();
-
-            return product;
-        }
-
-
-
-
+    return product;
+  }
 }
