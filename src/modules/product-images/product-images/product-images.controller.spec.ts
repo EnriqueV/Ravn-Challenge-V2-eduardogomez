05d9adb1ddc34a26f8da88fe10f7811e17ecbe31
EnriqueV <kalienrique40@gmail.com>
2022-03-11 17:20:15 -0600

    refactor: update product-images.controller.spec and product-images.service.spec

--- src/modules/product-images/product-images/product-images.controller.spec.ts
+++ src/modules/product-images/product-images/product-images.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+
+import { Images } from '../product-images.entity';
 import { ProductImagesController } from './product-images.controller';
+import { ProductImagesService } from './product-images.service';
 
 describe('ProductImagesController', () => {
   let controller: ProductImagesController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Images])],
       controllers: [ProductImagesController],
+      providers: [
+        ProductImagesService,
+        {
+          provide: getRepositoryToken(Images),
+          useValue: {
+            find: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductImagesController>(ProductImagesController);
--- src/modules/product-images/product-images/product-images.service.spec.ts
+++ src/modules/product-images/product-images/product-images.service.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { DeleteResult } from 'typeorm';
+
+// import { ProductFactory } from '../../../factories/products.factory';
+import { ImageFactory } from '../../../factories/images.factory';
+import { Images } from '../product-images.entity';
+import { Image } from './../dto/products-images.dto';
 import { ProductImagesService } from './product-images.service';
 
 describe('ProductImagesService', () => {
   let service: ProductImagesService;
+  // const productFactory: ProductFactory = new ProductFactory();
+  const imageFactory: ImageFactory = new ImageFactory();
+  let image: Image;
+  let images: Image[];
+  const delRes: DeleteResult = {
+    affected: 0,
+    raw: [],
+  };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    image = await imageFactory.make();
+    images = await imageFactory.makeMany(5);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductImagesService],
+      imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Images])],
+      providers: [
+        ProductImagesService,
+        {
+          provide: getRepositoryToken(Images),
+          useValue: {
+            find: jest.fn().mockResolvedValue(images),
+            findOne: jest.fn().mockResolvedValue(images?.[2]),
+            save: jest.fn().mockResolvedValue(image),
+            update: jest.fn().mockResolvedValue({
+              affected: 1,
+              raw: [],
+            }),
+            delete: jest.fn().mockResolvedValue(delRes),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ProductImagesService>(ProductImagesService);
