951bd8b99b8005198ea7bb437ea1952c3d364f47
EnriqueV <kalienrique40@gmail.com>
2022-03-11 16:34:59 -0600

    chore: update product.controller.spec

--- src/modules/products/products/products.controller.spec.ts
+++ src/modules/products/products/products.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+
+import { Product } from '../products.entity';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Product])],
       controllers: [ProductsController],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: {
+            find: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
